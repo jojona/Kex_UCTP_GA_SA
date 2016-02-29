ada719329dc23386ec1e337fb930a28e80ff2ad7
Johan <johan@jonasson.mobi>
2016-02-29 14:17:13 +0100

    First commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+	
+	public static void main(String [] args) {
+		
+		
+	}
+	
+	public void SA() {
+		SA sa = new SA();
+	}
+	
+	public void GA() {
+		GA ga = new GA();
+	}
+	
+}
+
